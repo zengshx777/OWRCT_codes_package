1664aea250ae76f0f3f24ec97f8c43d116d0f5cb
zengshx777 <34327225+zengshx777@users.noreply.github.com>
2020-03-31 11:30:15 -0400

    table_change

--- Main_RCT_Binary.R
+++ Main_RCT_Binary.R
@@ -68,175 +68,179 @@ n.grid=seq(50,200,by=10)
 
 
 for (n in n.grid){
-
-EST_DIFF<-SE_DIFF<-matrix(NA,nsim,4)
-colnames(EST_DIFF)<-colnames(SE_DIFF)<-c("UNADJ","IPW","OW","LR")
-
-EST_RISK<-SE_RISK<-matrix(NA,nsim,4)
-colnames(EST_RISK)<-colnames(SE_RISK)<-c("UNADJ","IPW","OW","LR")
-
-EST_ODDS<-SE_ODDS<-matrix(NA,nsim,4)
-colnames(EST_ODDS)<-colnames(SE_ODDS)<-c("UNADJ","IPW","OW","LR")
-
-##True Values
-true.p.1=numeric(nsim)
-true.p.0=numeric(nsim)
-
-for(i in 1:nsim){
-  x<-rmvnorm(n,rep(0,p),diag(1,p))
-  z<-rbinom(n,1,pt)
-  ##Use hypergeometric to ensure existence of treat/control
-  # z=rep(0,n)
-  # z[sample(1:n,round(n*pt),replace=F)]=1
-  bsq<-2/(1+2+2^2+2^3+2^4)
-  betasq<-rep(c(bsq,2*bsq,2^2*bsq,2^3*bsq,2^4*bsq),each=p/5)
   
-  deltasq<-1
-  betasq=(signal_level* deltasq^2)*betasq/sum(betasq)
-  beta<-sqrt(betasq) 
+  EST_DIFF<-SE_DIFF<-matrix(NA,nsim,4)
+  colnames(EST_DIFF)<-colnames(SE_DIFF)<-c("UNADJ","IPW","OW","LR")
   
-  ##
-  beta.tau=rep(1,p)
-  beta.tau=sqrt(signal_level* deltasq^2)*beta.tau
-
+  EST_RISK<-SE_RISK<-matrix(NA,nsim,4)
+  colnames(EST_RISK)<-colnames(SE_RISK)<-c("UNADJ","IPW","OW","LR")
   
-  ##Add second order term if misspecified
-  if (mis.specified==1){
-  x.inter=x[,1:(p-1)]*x[,2:p]
+  EST_ODDS<-SE_ODDS<-matrix(NA,nsim,4)
+  colnames(EST_ODDS)<-colnames(SE_ODDS)<-c("UNADJ","IPW","OW","LR")
   
-  beta.quad<-rep(bsq,p-1)
-  beta.quad=(signal_level* deltasq^2)*beta.quad/sum(beta.quad)
-  beta.quad=sqrt(beta.quad)
+  ##True Values
+  true.p.1=numeric(nsim)
+  true.p.0=numeric(nsim)
   
-  p.mean<-as.numeric(rare.degree+x%*%beta+x%*%beta.tau*z*h.degree+x.inter%*%beta.quad+gamma*z)
-  true.mean.1=as.numeric(rare.degree+x%*%beta+x%*%beta.tau*h.degree+x.inter%*%beta.quad+gamma)
-  true.mean.0=as.numeric(rare.degree+x%*%beta+x.inter%*%beta.quad)
-  ##Correctly Specified
-  }else{
-    p.mean<-as.numeric(rare.degree+x%*%beta+x%*%beta.tau*z*h.degree+gamma*z)
-    true.mean.1=as.numeric(rare.degree+x%*%beta+x%*%beta.tau*h.degree+gamma)
-    true.mean.0=as.numeric(rare.degree+x%*%beta)
+  for(i in 1:nsim){
+    x<-rmvnorm(n,rep(0,p),diag(1,p))
+    z<-rbinom(n,1,pt)
+    ##Use hypergeometric to ensure existence of treat/control
+    # z=rep(0,n)
+    # z[sample(1:n,round(n*pt),replace=F)]=1
+    bsq<-2/(1+2+2^2+2^3+2^4)
+    betasq<-rep(c(bsq,2*bsq,2^2*bsq,2^3*bsq,2^4*bsq),each=p/5)
     
+    deltasq<-1
+    betasq=(signal_level* deltasq^2)*betasq/sum(betasq)
+    beta<-sqrt(betasq) 
+    
+    ##
+    beta.tau=rep(1,p)
+    beta.tau=sqrt(signal_level* deltasq^2)*beta.tau
+    
+    
+    ##Add second order term if misspecified
+    if (mis.specified==1){
+      x.inter=x[,1:(p-1)]*x[,2:p]
+      
+      beta.quad<-rep(bsq,p-1)
+      beta.quad=(signal_level* deltasq^2)*beta.quad/sum(beta.quad)
+      beta.quad=sqrt(beta.quad)
+      
+      p.mean<-as.numeric(rare.degree+x%*%beta+x%*%beta.tau*z*h.degree+x.inter%*%beta.quad+gamma*z)
+      true.mean.1=as.numeric(rare.degree+x%*%beta+x%*%beta.tau*h.degree+x.inter%*%beta.quad+gamma)
+      true.mean.0=as.numeric(rare.degree+x%*%beta+x.inter%*%beta.quad)
+      ##Correctly Specified
+    }else{
+      p.mean<-as.numeric(rare.degree+x%*%beta+x%*%beta.tau*z*h.degree+gamma*z)
+      true.mean.1=as.numeric(rare.degree+x%*%beta+x%*%beta.tau*h.degree+gamma)
+      true.mean.0=as.numeric(rare.degree+x%*%beta)
+      
+    }
+    
+    W<-cbind(1,x)
+    
+    if(logit==1){
+      true.p.1[i]=mean(1/(1+exp(-true.mean.1)))
+      true.p.0[i]=mean(1/(1+exp(-true.mean.0)))
+      p.y=1/(1+exp(-p.mean))
+    }else{
+      true.p.1[i]=mean(pnorm(true.mean.1))
+      true.p.0[i]=mean(pnorm(true.mean.0))
+      p.y=pnorm(p.mean)
+    }
+    ###Generate Binary Outcome
+    ##Generate from Bernouli trial
+    ##True model is logit model
+    
+    
+    y=unlist(lapply(p.y,FUN=function(x){sample(c(1,0),1,prob=c(x,1-x))}))
+    
+    
+    
+    
+    # UNADJ
+    res.crude <- Crude(y=y, z=z, W=W,binary=1)
+    EST_DIFF[i,1] <- res.crude$mean_diff
+    EST_RISK[i,1] <- res.crude$log_risk_ratio
+    EST_ODDS[i,1] <- res.crude$log_odds_ratio
+    
+    SE_DIFF[i,1]<-res.crude$se_mean_diff
+    SE_RISK[i,1]<-res.crude$se_risk_ratio
+    SE_ODDS[i,1]<-res.crude$se_odds_ratio
+    
+    
+    # IPW
+    res.IPWC <- IPWC(y.all=y, z.all=z, W.all=W, q.all=0,binary=1)
+    EST_DIFF[i,2] <- res.IPWC$mean_diff
+    EST_RISK[i,2] <- res.IPWC$log_risk_ratio
+    EST_ODDS[i,2] <- res.IPWC$log_odds_ratio
+    
+    SE_DIFF[i,2]<-res.IPWC$se_mean_diff
+    SE_RISK[i,2]<-res.IPWC$se_risk_ratio
+    SE_ODDS[i,2]<-res.IPWC$se_odds_ratio
+    
+    # OW
+    res.OW <- OW(y=y, z=z, W=W,binary=1)
+    EST_DIFF[i,3] <- res.OW$mean_diff
+    EST_RISK[i,3] <- res.OW$log_risk_ratio
+    EST_ODDS[i,3] <- res.OW$log_odds_ratio
+    
+    SE_DIFF[i,3]<-res.OW$se_mean_diff
+    SE_RISK[i,3]<-res.OW$se_risk_ratio
+    SE_ODDS[i,3]<-res.OW$se_odds_ratio
+    
+    #LR
+    #Supply the one without intercept
+    res.LR <- LR(y=y, z=z, W=x,binary=1)
+    #NA happen if not converged
+    EST_DIFF[i,4] <- res.LR$mean_diff
+    EST_RISK[i,4] <- res.LR$log_risk_ratio
+    EST_ODDS[i,4] <- res.LR$log_odds_ratio
+    
+    SE_DIFF[i,4]<-res.LR$se_mean_diff
+    SE_RISK[i,4]<-res.LR$se_risk_ratio
+    SE_ODDS[i,4]<-res.LR$se_odds_ratio
+    
+    #DR
+    #Supply the one without intercept
+    #Not supported binary currently
+    # res.DR <- DR(y=y, z=z, W=x)
+    # EST[i,5] <- res.DR$tau
+    # SE[i,5] <- res.DR$se
+    
+    
+    if(i%%500==0)print(i)
   }
   
-  W<-cbind(1,x)
-  
-  if(logit==1){
-  true.p.1[i]=mean(1/(1+exp(-true.mean.1)))
-  true.p.0[i]=mean(1/(1+exp(-true.mean.0)))
-  p.y=1/(1+exp(-p.mean))
-  }else{
-  true.p.1[i]=mean(pnorm(true.mean.1))
-  true.p.0[i]=mean(pnorm(true.mean.0))
-  p.y=pnorm(p.mean)
-  }
-  ###Generate Binary Outcome
-  ##Generate from Bernouli trial
-  ##True model is logit model
-
-
-  y=unlist(lapply(p.y,FUN=function(x){sample(c(1,0),1,prob=c(x,1-x))}))
-
-  
-
-  
-  # UNADJ
-  res.crude <- Crude(y=y, z=z, W=W,binary=1)
-  EST_DIFF[i,1] <- res.crude$mean_diff
-  EST_RISK[i,1] <- res.crude$log_risk_ratio
-  EST_ODDS[i,1] <- res.crude$log_odds_ratio
-  
-  SE_DIFF[i,1]<-res.crude$se_mean_diff
-  SE_RISK[i,1]<-res.crude$se_risk_ratio
-  SE_ODDS[i,1]<-res.crude$se_odds_ratio
   
   
-  # IPW
-  res.IPWC <- IPWC(y.all=y, z.all=z, W.all=W, q.all=0,binary=1)
-  EST_DIFF[i,2] <- res.IPWC$mean_diff
-  EST_RISK[i,2] <- res.IPWC$log_risk_ratio
-  EST_ODDS[i,2] <- res.IPWC$log_odds_ratio
+  # # Results
+  complete.id=which(apply(EST_DIFF,1,FUN=function(x){all(!is.na(x))}))
   
-  SE_DIFF[i,2]<-res.IPWC$se_mean_diff
-  SE_RISK[i,2]<-res.IPWC$se_risk_ratio
-  SE_ODDS[i,2]<-res.IPWC$se_odds_ratio
-
-  # OW
-  res.OW <- OW(y=y, z=z, W=W,binary=1)
-  EST_DIFF[i,3] <- res.OW$mean_diff
-  EST_RISK[i,3] <- res.OW$log_risk_ratio
-  EST_ODDS[i,3] <- res.OW$log_odds_ratio
+  ##Use MC approximate true parameter
+  true_diff=mean(true.p.1)-mean(true.p.0)
+  true_risk=log(mean(true.p.1)/mean(true.p.0))
+  true_odds=log(mean(true.p.1)/(1-mean(true.p.1)))-log(mean(true.p.0)/(1-mean(true.p.0)))
   
-  SE_DIFF[i,3]<-res.OW$se_mean_diff
-  SE_RISK[i,3]<-res.OW$se_risk_ratio
-  SE_ODDS[i,3]<-res.OW$se_odds_ratio
+  #Remove non finite one
+  EST_DIFF[is.infinite(EST_DIFF)]=NA
+  EST_RISK[is.infinite(EST_RISK)]=NA
+  EST_ODDS[is.infinite(EST_ODDS)]=NA
   
-  #LR
-  #Supply the one without intercept
-  res.LR <- LR(y=y, z=z, W=x,binary=1)
-  #NA happen if not converged
-  EST_DIFF[i,4] <- res.LR$mean_diff
-  EST_RISK[i,4] <- res.LR$log_risk_ratio
-  EST_ODDS[i,4] <- res.LR$log_odds_ratio
+  RMSE_DIFF=rbind(RMSE_DIFF,c(n,sqrt(colMeans((EST_DIFF[complete.id,]-true_diff)^2,na.rm=T))))
+  RMSE_RISK=rbind(RMSE_RISK,c(n,sqrt(colMeans((EST_RISK[complete.id,]-true_risk)^2,na.rm=T))))
+  RMSE_ODDS=rbind(RMSE_ODDS,c(n,sqrt(colMeans((EST_ODDS[complete.id,]-true_odds)^2,na.rm=T))))
   
-  SE_DIFF[i,4]<-res.LR$se_mean_diff
-  SE_RISK[i,4]<-res.LR$se_risk_ratio
-  SE_ODDS[i,4]<-res.LR$se_odds_ratio
+  MC_SD_DIFF=rbind(MC_SD_DIFF,c(n,apply(EST_DIFF[complete.id,],2,FUN=function(x){sd(x,na.rm = T)})))
+  MC_SD_RISK=rbind(MC_SD_RISK,c(n,apply(EST_RISK[complete.id,],2,FUN=function(x){sd(x,na.rm = T)})))
+  MC_SD_ODDS=rbind(MC_SD_ODDS,c(n,apply(EST_ODDS[complete.id,],2,FUN=function(x){sd(x,na.rm = T)})))
   
-  #DR
-  #Supply the one without intercept
-  #Not supported binary currently
-  # res.DR <- DR(y=y, z=z, W=x)
-  # EST[i,5] <- res.DR$tau
-  # SE[i,5] <- res.DR$se
+  SE_DIFF[SE_DIFF>100]=NA
+  SE_RISK[SE_RISK>100]=NA
+  SE_ODDS[SE_ODDS>100]=NA
   
+  EST_SD_DIFF=rbind(EST_SD_DIFF,c(n,colMeans(SE_DIFF[complete.id,],na.rm=T)))
+  EST_SD_RISK=rbind(EST_SD_RISK,c(n,colMeans(SE_RISK[complete.id,],na.rm=T)))
+  EST_SD_ODDS=rbind(EST_SD_ODDS,c(n,colMeans(SE_ODDS[complete.id,],na.rm=T)))
   
-  if(i%%500==0)print(i)
-}
-
-
-
-# # Results
-complete.id=which(apply(EST_DIFF,1,FUN=function(x){all(!is.na(x))}))
-
-##Use MC approximate true parameter
-true_diff=mean(true.p.1)-mean(true.p.0)
-true_risk=log(mean(true.p.1)/mean(true.p.0))
-true_odds=log(mean(true.p.1)/(1-mean(true.p.1)))-log(mean(true.p.0)/(1-mean(true.p.0)))
-
-#Remove non finite one
-EST_DIFF[is.infinite(EST_DIFF)]=NA
-EST_RISK[is.infinite(EST_RISK)]=NA
-EST_ODDS[is.infinite(EST_ODDS)]=NA
-
-RMSE_DIFF=rbind(RMSE_DIFF,c(n,sqrt(colMeans((EST_DIFF[complete.id,]-true_diff)^2,na.rm=T))))
-RMSE_RISK=rbind(RMSE_RISK,c(n,sqrt(colMeans((EST_RISK[complete.id,]-true_risk)^2,na.rm=T))))
-RMSE_ODDS=rbind(RMSE_ODDS,c(n,sqrt(colMeans((EST_ODDS[complete.id,]-true_odds)^2,na.rm=T))))
-
-MC_SD_DIFF=rbind(MC_SD_DIFF,c(n,apply(EST_DIFF[complete.id,],2,FUN=function(x){sd(x,na.rm = T)})))
-MC_SD_RISK=rbind(MC_SD_RISK,c(n,apply(EST_RISK[complete.id,],2,FUN=function(x){sd(x,na.rm = T)})))
-MC_SD_ODDS=rbind(MC_SD_ODDS,c(n,apply(EST_ODDS[complete.id,],2,FUN=function(x){sd(x,na.rm = T)})))
-
-EST_SD_DIFF=rbind(EST_SD_DIFF,c(n,colMeans(SE_DIFF[complete.id,],na.rm=T)))
-EST_SD_RISK=rbind(EST_SD_RISK,c(n,colMeans(SE_RISK[complete.id,],na.rm=T)))
-EST_SD_ODDS=rbind(EST_SD_ODDS,c(n,colMeans(SE_ODDS[complete.id,],na.rm=T)))
-
-CRATE_DIFF=rbind(CRATE_DIFF,c(n,colMeans(matrix(true_diff, nrow(EST_DIFF), ncol(EST_DIFF)) <= EST_DIFF + qnorm(0.975)*SE_DIFF & 
-                                 matrix(true_diff, nrow(EST_DIFF), ncol(EST_DIFF)) >= EST_DIFF - qnorm(0.975)*SE_DIFF,na.rm=T)))
-
-CRATE_RISK=rbind(CRATE_RISK,c(n,colMeans(matrix(true_risk, nrow(EST_RISK), ncol(EST_RISK)) <= EST_RISK + qnorm(0.975)*SE_RISK & 
-                                           matrix(true_risk, nrow(EST_RISK), ncol(EST_RISK)) >= EST_RISK - qnorm(0.975)*SE_RISK,na.rm=T)))
-
-CRATE_ODDS=rbind(CRATE_ODDS,c(n,colMeans(matrix(true_odds, nrow(EST_ODDS), ncol(EST_ODDS)) <= EST_ODDS + qnorm(0.975)*SE_ODDS & 
-                                           matrix(true_odds, nrow(EST_ODDS), ncol(EST_ODDS)) >= EST_ODDS - qnorm(0.975)*SE_ODDS,na.rm=T)))
-
-BIAS_DIFF=rbind(BIAS_DIFF,c(n,abs(colMeans(EST_DIFF[complete.id,])-true_diff)))
-BIAS_RISK=rbind(BIAS_RISK,c(n,abs(colMeans(EST_RISK[complete.id,])-true_risk)))
-BIAS_ODDS=rbind(BIAS_ODDS,c(n,abs(colMeans(EST_ODDS[complete.id,])-true_odds)))
-#Non convergence time
-NON_CONVER=rbind(NON_CONVER,c(n,nsim-length(complete.id)))
-
-print(paste(n," finished"))
+  CRATE_DIFF=rbind(CRATE_DIFF,c(n,colMeans(matrix(true_diff, nrow(EST_DIFF), ncol(EST_DIFF)) <= EST_DIFF + qnorm(0.975)*SE_DIFF & 
+                                             matrix(true_diff, nrow(EST_DIFF), ncol(EST_DIFF)) >= EST_DIFF - qnorm(0.975)*SE_DIFF,na.rm=T)))
+  
+  CRATE_RISK=rbind(CRATE_RISK,c(n,colMeans(matrix(true_risk, nrow(EST_RISK), ncol(EST_RISK)) <= EST_RISK + qnorm(0.975)*SE_RISK & 
+                                             matrix(true_risk, nrow(EST_RISK), ncol(EST_RISK)) >= EST_RISK - qnorm(0.975)*SE_RISK,na.rm=T)))
+  
+  CRATE_ODDS=rbind(CRATE_ODDS,c(n,colMeans(matrix(true_odds, nrow(EST_ODDS), ncol(EST_ODDS)) <= EST_ODDS + qnorm(0.975)*SE_ODDS & 
+                                             matrix(true_odds, nrow(EST_ODDS), ncol(EST_ODDS)) >= EST_ODDS - qnorm(0.975)*SE_ODDS,na.rm=T)))
+  
+  BIAS_DIFF=rbind(BIAS_DIFF,c(n,abs(colMeans(EST_DIFF[complete.id,])-true_diff)))
+  BIAS_RISK=rbind(BIAS_RISK,c(n,abs(colMeans(EST_RISK[complete.id,])-true_risk)))
+  BIAS_ODDS=rbind(BIAS_ODDS,c(n,abs(colMeans(EST_ODDS[complete.id,])-true_odds)))
+  #Non convergence time
+  NON_CONVER=rbind(NON_CONVER,c(n,nsim-length(complete.id)))
+  
+  print(paste(n," finished"))
 }
 ##Save data 
 setwd("binary/")
@@ -246,6 +250,6 @@ save(RMSE_RISK,RMSE_DIFF,RMSE_ODDS,
      CRATE_DIFF,CRATE_ODDS,CRATE_RISK,
      BIAS_DIFF,BIAS_RISK,BIAS_ODDS,NON_CONVER,
      file=paste(rare.degree,h.degree,pt,mis.specified,"binary_result.RData",sep="_"))
-##Make Figures
-setwd("..")
-source("plot_results_binary.R")
+# ##Make Figures
+# setwd("..")
+# source("plot_results_binary.R")
--- table_produce.R
+++ table_produce.R
@@ -3,9 +3,10 @@ library(xtable)
 helper<-function(results,BIAS,MC_SD,EST_SD,CRATE,sample_index)
 {
   results=rbind(results,cbind(
-    MC_SD[sample_index,c(1,2,3,5,4)],
-    EST_SD[sample_index,c(2,3,5,4)],
-    CRATE[sample_index,c(2,3,5,4)]))
+    MC_SD[sample_index,1],
+    (MC_SD[sample_index,2]/MC_SD[sample_index,c(3,5,4)])^2,
+    (EST_SD[sample_index,c(3,5,4)]/MC_SD[sample_index,c(3,5,4)])^2,
+    CRATE[sample_index,c(3,5,4)]))
   return(results)
 }
 sample_index=c(1,6,16)
@@ -65,14 +66,8 @@ mis.specified=1
 load(paste(h.degree,pt,mis.specified,"cont_result.RData",sep="_"))
 results_one=helper(results_one,BIAS,MC_SD,EST_SD,CRATE,sample_index)
 
-h.degree=0.75
-pt=0.7
-mis.specified=0
-load(paste(h.degree,pt,mis.specified,"cont_result.RData",sep="_"))
-results_one=helper(results_one,BIAS,MC_SD,EST_SD,CRATE,sample_index)
 
-
-print(xtable(results_one,digits=c(0,0,rep(3,12))),include.rownames = F)
+print(xtable(results_one,digits=c(0,0,rep(3,9))),include.rownames = F)
 
 #########Table 2
 results_two=NULL
@@ -169,9 +164,10 @@ results_two=helper(results_two,BIAS_DIFF,MC_SD_DIFF,EST_SD_DIFF,CRATE_DIFF,sampl
 results_two=helper(results_two,BIAS_RISK,MC_SD_RISK,EST_SD_RISK,CRATE_RISK,sample_index)
 results_two=helper(results_two,BIAS_ODDS,MC_SD_ODDS,EST_SD_ODDS,CRATE_ODDS,sample_index)
 
-print(xtable(results_two,digits=c(0,0,rep(3,12))),include.rownames = F)
+print(xtable(results_two,digits=c(0,0,rep(3,9))),include.rownames = F)
 
 ######################Table 3
+sample_index=1:16
 Failure_times=NULL
 rare.degree=0
 h.degree=0
@@ -201,5 +197,6 @@ pt=0.5
 mis.specified=0
 load(paste(rare.degree,h.degree,pt,mis.specified,"binary_result.RData",sep="_"))
 Failure_times=cbind(Failure_times,NON_CONVER[sample_index,2])
-print(xtable(Failure_times))
+#Failure_times[,2:5]=Failure_times[,2:5]/20
+print(xtable(Failure_times,digits=rep(0,6)),include.rownames = F)
       

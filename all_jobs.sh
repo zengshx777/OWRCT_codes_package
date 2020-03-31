bd9230bf19585834d09ccd3535ceb33b06448087
zengshx777 <34327225+zengshx777@users.noreply.github.com>
2020-03-31 13:15:42 -0400

    Update all_jobs.sh

--- all_jobs.sh
+++ all_jobs.sh
@@ -7,7 +7,6 @@ R CMD BATCH --vanilla '--args h.degree=0.75 pt=0.5 mis.specified=0 rand.seed=2' 
 R CMD BATCH --vanilla '--args h.degree=0 pt=0.7 mis.specified=0 rand.seed=3' Main_RCT_Continuous.R cont_3.out &
 R CMD BATCH --vanilla '--args h.degree=0 pt=0.5 mis.specified=1 rand.seed=4' Main_RCT_Continuous.R cont_4.out &
 R CMD BATCH --vanilla '--args h.degree=0 pt=0.7 mis.specified=1 rand.seed=5' Main_RCT_Continuous.R cont_5.out &
-R CMD BATCH --vanilla '--args h.degree=0.75 pt=0.7 mis.specified=0 rand.seed=6' Main_RCT_Continuous.R cont_6_out &
 R CMD BATCH --vanilla '--args h.degree=0.25 pt=0.5 mis.specified=0 rand.seed=7' Main_RCT_Continuous.R cont_7_out &
 R CMD BATCH --vanilla '--args h.degree=0.5 pt=0.5 mis.specified=0 rand.seed=8' Main_RCT_Continuous.R cont_8_out &
 R CMD BATCH --vanilla '--args h.degree=0 pt=0.6 mis.specified=0 rand.seed=9' Main_RCT_Continuous.R cont_9_out &
